9c211005d4c8f4f8a370cd7a07b2ff8fa0be2fb0
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-06-04 18:57:34 -0300

    configuração da startup

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificação de erros
+
+        // validação de modelstate
+
+        //validação da operação de negocios
+    }
+}
