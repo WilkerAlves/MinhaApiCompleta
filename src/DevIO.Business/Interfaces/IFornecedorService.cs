9c0713fc52c19fecefa5f420e096ea20904206c7
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-06-05 13:58:22 -0300

    Escrevendo as rotas para adicionar, editar, listar e deletar fornecedores

--- src/DevIO.Business/Interfaces/IFornecedorService.cs
+++ src/DevIO.Business/Interfaces/IFornecedorService.cs
@@ -1,16 +1,14 @@
 ﻿using DevIO.Business.Models;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DevIO.Business.Interfaces
 {
     public interface IFornecedorService : IDisposable
     {
-        Task Adicionar(Fornecedor fornecedor);
-        Task Atualizar(Fornecedor fornecedor);
-        Task Remover(Guid id);
+        Task<bool> Adicionar(Fornecedor fornecedor);
+        Task<bool> Atualizar(Fornecedor fornecedor);
+        Task<bool> Remover(Guid id);
         Task AtualizarEndereco(Endereco endereco);
     }
 }
